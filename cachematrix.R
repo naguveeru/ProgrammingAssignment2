5442eb90ede51922d5809a87d64222d88f80009c
naguveeru <naguveeru@gmail.com>
2015-04-24 12:36:25 -0400

    Update cachematrix.R
    Saving the R code directly on the gitHub

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    invmatrix = NULL
+    set<- function(y) { if((is.matrix(x)==TRUE) &&  (all.equal(y,x))==TRUE) {
+                                print('This matrix is already set') }
+                        else { invmatrix <<- NULL; x <<- y }  
+                      }
+    get <- function() {return(x)}
+    setinv <- function(y){invmatrix <<- y}
+    getinv <-function() {invmatrix}
+    list(set=set, get=get,setinv=setinv,getinv=getinv)
 
 }
 
@@ -12,4 +21,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(x$getinv())) {
+            print('Inverse of this matrix exists, fetching from cache')
+        return(x$getinv())
+        }
+        n<-tryCatch(
+                 solve(x$get())
+                 ,warning=function(x){'Error'}
+                 ,error=function(x){'Error'}
+                 ,finally={""}
+        )
+        if(!is.matrix(n) && n == "Error") {
+                print  ('Error inverting, check if invertible matrix')  
+                return (NULL)
+        }
+        x$setinv(n)
+        return (n)
 }
