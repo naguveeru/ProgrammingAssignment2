caf3a545ccd166524f753dd5f9bf05d93f2227c9
naguveeru <naguveeru@gmail.com>
2015-04-26 00:45:56 -0400

    updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ## This functions here are for computing and caching the inverse of the matrix
-## makeCacheMatrix : This will function will create a object which has list of 4 functions - 
+## makeCacheMatrix : This function will create a object which has list of 4 functions - 
 ##      1. set(y)    = to set the matrix whose inverse needs to be cached. Input 'y' is the original matrix.
 ##      2. get()     = to get the matrix whose inverse is cached. Return is a matrix object.
 ##      3. setinv(y) = to set the inverse of the matrix x. input 'y' is the inverse matrix that is supplied as input.
 ##      4. getinv()  = to get the inverse of the matrix if cached. Retrun is a metrix object.
-##   If you try to set the same matrix again, this function will print a message that matrix is already set. 
+##   If you try to set the same matrix again, a message will be printed stating that matrix is already set. 
 ##   This will avoid resetting the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -20,10 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## cacheSolve   : this function will take the object of type makeCacheMatrix and compute and cache the matrix inverse. 
-##                If the matrix inverse is already cached, it will be fetcehd from cached and not computed again.
+## cacheSolve   : this function will take the object of type makeCacheMatrix and cache the inverse matrix. 
+##                If the matrix inverse is already cached, it will be fetched from cache and not computed again.
 ##   Exception- If the supplied matrix is not invertible, the function will print error.
-##   Return - if the matrix is invertible , function returns the inverse matrix. Ohterwise NULL.
+##   Return - if the matrix is invertible , function returns the inverse matrix. Otherwise NULL.
 ## 
 
 cacheSolve <- function(x, ...) {
